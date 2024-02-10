15410e41dabc946ac56209a1c61e4c063d419aa3
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-02-10 18:48:46 +0200

    add all the code into app.js

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
+
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public", 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'admin', 'index.html'));
+});
+app.get('/getorders', (req,res) => {
+    let fileData = JSON.parse(fs.readFileSync('orders.txt', 'utf-8'));
+    res.json(fileData);
 })
-app.post('/save-order', (req, res) =>{
+app.post('/save-order', (req, res) => {
     const data = req.body;
     console.log(data);
+    let fileData = JSON.parse(fs.readFileSync('orders.txt', 'utf-8'));
+    console.log(fileData)
+    fileData.push({
+        name: data.name,
+        phone: data.phone,
+        list: data.list
+    })
+    fs.writeFile('orders.txt', JSON.stringify(fileData), (err)=>{
+        if(err){
+            console.log('err')
+        }else{
+            console.log(`Замовлення збережено`)
+        }
+    })
+
+    res.sendStatus(200);
+
 })
-app.listen(PORT, ()=>{
-    console.log(`Server work on PORT: ${PORT}`);
+
+app.listen(PORT, () => {
+    console.log(`Server work on PORT: ${PORT}`)
 })
+
