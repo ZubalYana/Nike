ebb05345e92ceac8bf9a6f1329c404c756ef5ff7
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-02-26 19:50:19 +0200

    add login.html page

--- app.js
+++ app.js
@@ -1,8 +1,65 @@
 const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 const path = require('path');
 const fs = require('fs');
+
+
+const users = [
+    { id: 1, username: 'admin', password: '0000' },
+    { id: 2, username: 'vitaliy', password: '1111' },
+];
+passport.use(new LocalStrategy((username, password, done) => {
+    const user = users.find(u => u.username == username && u.password == password);
+    if (user) {
+        return done(null, user);
+    } else {
+        return done(null, false, { message: 'Невірний логін або пароль' });
+    }
+}))
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+passport.deserializeUser((id, done) => {
+    const user = users.find(u => u.id == id);
+    done(null, user);
+})
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'secret-key', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/admin',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/admin', isLoggedIn, (req, res) => {
+    res.send('Ви ввійшли в адмін панель');
+})
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+})
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+}
+
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Something went wrong!');
+})
+
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
